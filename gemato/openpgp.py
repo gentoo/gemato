gentoo/gemato

commit 4a6d2c0daad479ada7cb8492e4c900cc0f08dad4

openpgp: Make list_keys() return all UIDs
Modify list_keys() to return all UIDs in a key, and do filtering
in refresh_keys_wkd().  This will enable us to cleanly reuse it
to check for keys without (valid) UIDs.

Signed-off-by: Michał Górny <mgorny@gentoo.org>

gemato/openpgp.py
@@ -140,7 +140,8 @@ class SystemGPGEnvironment:
         raise NotImplementedError(
             'import_key() is not implemented by this OpenPGP provider')
 
-    def list_keys(self, key_ids: list[str] = []) -> dict[str, list[str]]:
+    def list_keys(self, key_ids: list[str] = []
+                  ) -> dict[str, list[tuple[str, str]]]:
         """
         List fingerprints and UIDs of specified keys or all keys in keyring
 
@@ -182,15 +183,8 @@ class SystemGPGEnvironment:
                     raise OpenPGPKeyListingError(
                         f'UID without key in GPG output: {line}')
                 uid = line.split(b':')[9]
-                _, addr = email.utils.parseaddr(
-                    uid.decode('utf8', errors='replace'))
-                if '@' in addr:
-                    LOGGER.debug(f'list_keys(): UID: {addr}')
-                    ret[fpr].append(addr)
-                else:
-                    LOGGER.debug(
-                        f'list_keys(): ignoring UID without mail: '
-                        f'{uid!r}')
+                LOGGER.debug(f'list_keys(): UID: {uid}')
+                ret[fpr].append(uid)
 
         return ret
 
@@ -571,12 +565,19 @@ debug-level guru
             return False
         addrs = set()
         for key, uids in keys.items():
-            if not uids:
+            key_addrs = {
+                addr for uid in uids
+                if "@" in (addr := email.utils.parseaddr(
+                    uid.decode("utf8", errors="replace")
+                )[1])
+            }
+            print(key_addrs)
+            if not key_addrs:
                 LOGGER.debug(
-                    f'refresh_keys_wkd(): failing due to no UIDs on '
-                    f'key {key}')
+                    f"refresh_keys_wkd(): failing due to no email addresses "
+                    f"on key {key}")
                 return False
-            addrs.update(uids)
+            addrs.update(key_addrs)
         expected_keys = frozenset(keys)
 
         data = b''

tests/test_openpgp.py
@@ -739,12 +739,16 @@ def test_recursive_manifest_loader_save_submanifest(tmp_path, privkey_env):
 
 @pytest.mark.parametrize(
     'key_var,expected',
-    [('VALID_PUBLIC_KEY', {KEY_FINGERPRINT: ['gemato@example.com']}),
+    [('VALID_PUBLIC_KEY',
+      {KEY_FINGERPRINT: [b"gemato test key <gemato@example.com>"]}),
      ('OTHER_VALID_PUBLIC_KEY',
-      {OTHER_KEY_FINGERPRINT: ['gemato@example.com']}),
-     ('VALID_KEY_SUBKEY', {KEY_FINGERPRINT: ['gemato@example.com']}),
-     ('VALID_KEY_NOEMAIL', {KEY_FINGERPRINT: []}),
-     ('VALID_KEY_NONUTF', {KEY_FINGERPRINT: ['gemato@example.com']}),
+      {OTHER_KEY_FINGERPRINT: [b"gemato test key <gemato@example.com>"]}),
+     ('VALID_KEY_SUBKEY',
+      {KEY_FINGERPRINT: [b"gemato test key <gemato@example.com>"]}),
+     ('VALID_KEY_NOEMAIL',
+      {KEY_FINGERPRINT: [b"gemato test key"]}),
+     ('VALID_KEY_NONUTF',
+      {KEY_FINGERPRINT: [b"gemat\xf6 test key <gemato@example.com>"]}),
      ])
 def test_list_keys(openpgp_env, key_var, expected):
     try:

commit 2fcd5aef53627fd668a3ef69ae5d14d1ddd47d45

openpgp: drop debugging print() from list_keys
Fixes: 4a6d2c0daad479ada7cb8492e4c900cc0f08dad4
Signed-off-by: Sam James <sam@gentoo.org>

gemato/openpgp.py
@@ -585,7 +585,6 @@ debug-level guru
                     uid.decode("utf8", errors="replace")
                 )[1])
             }
-            print(key_addrs)
             if not key_addrs:
                 LOGGER.debug(
                     f"refresh_keys_wkd(): failing due to no email addresses "